9bab3a5f08cb0b60a8ebbc65e08a071a672b6416
Jehaan Jacob Joseph <joseph@ihp-microelectronics.com>
2025-02-17 17:47:25 +0100

    first commit

--- mac.sv
+++ mac.sv
@@ -27,11 +27,12 @@ module mac (
     output logic signed [18:0] o_res;                                          // Output Result
 
     logic signed [15:0] mul2add_wire_0, mul2add_wire_1, mul2add_wire_2, mul2add_wire_3, mul2add_wire_4, mul2add_wire_5, mul2add_wire_6, mul2add_wire_7;
-    logic signed [15:0] mul2add_reg_0, mul2add_reg_1, mul2add_reg_2, mul2add_reg_3, mul2add_reg_4, mul2add_reg_5, mul2add_reg_6, mul2add_reg_7;
-    logic signed [18:0] o_sum, o_sum_reg;
+    logic signed [18:0] o_sum;
 
     multiplier_array mult_array
         (
+            .i_nrst(i_nrst),
+            .i_clk(i_clk),
             .i_a0(i_a0),
             .i_a1(i_a1),
             .i_a2(i_a2),
@@ -62,42 +63,18 @@ module mac (
         (
             .i_nrst(i_nrst),
             .i_clk(i_clk),
-            .i_0(mul2add_reg_0),
-            .i_1(mul2add_reg_1),
-            .i_2(mul2add_reg_2),
-            .i_3(mul2add_reg_3),
-            .i_4(mul2add_reg_4),
-            .i_5(mul2add_reg_5),
-            .i_6(mul2add_reg_6),
-            .i_7(mul2add_reg_7),
+            .i_0(mul2add_wire_0),
+            .i_1(mul2add_wire_1),
+            .i_2(mul2add_wire_2),
+            .i_3(mul2add_wire_3),
+            .i_4(mul2add_wire_4),
+            .i_5(mul2add_wire_5),
+            .i_6(mul2add_wire_6),
+            .i_7(mul2add_wire_7),
             .o_sum(o_sum)
         ); 
-
-    always_ff @( posedge i_clk ) begin
-        if ( !i_nrst ) begin
-            mul2add_reg_0 <= 0;
-            mul2add_reg_1 <= 0;
-            mul2add_reg_2 <= 0;
-            mul2add_reg_3 <= 0;
-            mul2add_reg_4 <= 0;
-            mul2add_reg_5 <= 0;
-            mul2add_reg_6 <= 0;
-            mul2add_reg_7 <= 0;
-            o_sum_reg <= 0;
-        end else begin
-            mul2add_reg_0 <= mul2add_wire_0;
-            mul2add_reg_1 <= mul2add_wire_1;
-            mul2add_reg_2 <= mul2add_wire_2;
-            mul2add_reg_3 <= mul2add_wire_3;
-            mul2add_reg_4 <= mul2add_wire_4;
-            mul2add_reg_5 <= mul2add_wire_5;
-            mul2add_reg_6 <= mul2add_wire_6;
-            mul2add_reg_7 <= mul2add_wire_7;
-            o_sum_reg <= o_sum;
-        end
-    end
     
-    assign o_res = o_sum_reg;
+    assign o_res = o_sum;
 
 endmodule
 
@@ -128,6 +105,7 @@ module adder_tree (
 
     // Intermediate Registers
     logic signed [16:0] sum_stage_0_0_reg, sum_stage_0_1_reg, sum_stage_0_2_reg, sum_stage_0_3_reg; // Sums of Stage 0
+    logic signed [17:0] sum_stage_2_0_reg; // Sums of Stage 2 
     
     // Stage 0
     adder #(.BITWIDTH(16)) add_stage_0_0
@@ -187,11 +165,13 @@ module adder_tree (
             sum_stage_0_1_reg <= 0;
             sum_stage_0_2_reg <= 0;
             sum_stage_0_3_reg <= 0;
+            sum_stage_2_0_reg <= 0;
         end else begin
             sum_stage_0_0_reg <= sum_stage_0_0;
             sum_stage_0_1_reg <= sum_stage_0_1;
             sum_stage_0_2_reg <= sum_stage_0_2;
             sum_stage_0_3_reg <= sum_stage_0_3;
+            sum_stage_2_0_reg <= sum_stage_2_0;
         end
     end
 
@@ -201,6 +181,8 @@ endmodule
 
 // Multiplier Module Definition
 module multiplier_array (
+        i_nrst,
+        i_clk,
         i_a0,
         i_a1,
         i_a2,
@@ -227,65 +209,101 @@ module multiplier_array (
         o_7
     );
 
+    input                      i_nrst;
+    input                      i_clk;
     input  logic signed  [7:0] i_a0, i_a1, i_a2, i_a3, i_a4, i_a5, i_a6, i_a7; // Input group A
     input  logic signed  [7:0] i_b0, i_b1, i_b2, i_b3, i_b4, i_b5, i_b6, i_b7; // Input group B
-    output logic signed [15:0] o_0, o_1, o_2, o_3, o_4, o_5, o_6, o_7;
+    output logic signed [15:0] o_0, o_1, o_2, o_3, o_4, o_5, o_6, o_7; // Products
+
+    logic signed [15:0] o_0_wire, o_1_wire, o_2_wire, o_3_wire, o_4_wire, o_5_wire, o_6_wire, o_7_wire;
+    logic signed [15:0] o_0_reg, o_1_reg, o_2_reg, o_3_reg, o_4_reg, o_5_reg, o_6_reg, o_7_reg;
 
     multiplier mult_0
         (
             .i_a(i_a0),
             .i_b(i_b0),
-            .o_p(o_0)
+            .o_p(o_0_wire)
         );
 
     multiplier mult_1
         (
             .i_a(i_a1),
             .i_b(i_b1),
-            .o_p(o_1)
+            .o_p(o_1_wire)
         );
 
     multiplier mult_2
         (
             .i_a(i_a2),
             .i_b(i_b2),
-            .o_p(o_2)
+            .o_p(o_2_wire)
         );
 
     multiplier mult_3
         (
             .i_a(i_a3),
             .i_b(i_b3),
-            .o_p(o_3)
+            .o_p(o_3_wire)
         );
 
     multiplier mult_4
         (
             .i_a(i_a4),
             .i_b(i_b4),
-            .o_p(o_4)
+            .o_p(o_4_wire)
         );
 
     multiplier mult_5
         (
             .i_a(i_a5),
             .i_b(i_b5),
-            .o_p(o_5)
+            .o_p(o_5_wire)
         );
 
     multiplier mult_6
         (
             .i_a(i_a6),
             .i_b(i_b6),
-            .o_p(o_6)
+            .o_p(o_6_wire)
         );
 
     multiplier mult_7
         (
             .i_a(i_a7),
             .i_b(i_b7),
-            .o_p(o_7)
+            .o_p(o_7_wire)
         );
+
+    always_ff @( posedge i_clk ) begin
+        if ( !i_nrst ) begin
+            o_0_reg <= 0;
+            o_1_reg <= 0; 
+            o_2_reg <= 0; 
+            o_3_reg <= 0; 
+            o_4_reg <= 0; 
+            o_5_reg <= 0; 
+            o_6_reg <= 0; 
+            o_7_reg <= 0; 
+        end else begin
+            o_0_reg <= o_0_wire;
+            o_1_reg <= o_1_wire; 
+            o_2_reg <= o_2_wire; 
+            o_3_reg <= o_3_wire; 
+            o_4_reg <= o_4_wire; 
+            o_5_reg <= o_5_wire; 
+            o_6_reg <= o_6_wire; 
+            o_7_reg <= o_7_wire; 
+        end
+    end
+
+    assign o_0 = o_0_reg;
+    assign o_1 = o_1_reg;
+    assign o_2 = o_2_reg;
+    assign o_3 = o_3_reg;
+    assign o_4 = o_4_reg;
+    assign o_5 = o_5_reg;
+    assign o_6 = o_6_reg;
+    assign o_7 = o_7_reg;
     
 endmodule
 
